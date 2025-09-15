fb67537c580e7ae1141d909b9aafb040205b619f
Neil Shah-Quinn <nshahquinn@wikimedia.org>
2025-09-15 23:50:23 +0000

    Make version 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="nshahquinn",
-    version="0.1",
+    version="1.0",
     description="Neil Shah-Quinn's personal Python stuff",
     python_requires=">=3",
     author="Neil Shah-Quinn",
